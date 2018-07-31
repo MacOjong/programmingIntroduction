d58296fe6e6e63ae37bb563456af93b3369957c1
patricksile <patricksile@gmail.com>
2018-07-31 03:07:32 +0100

    result and count variable added

--- reverse_string.py
+++ reverse_string.py
@@ -2,11 +2,21 @@
 # // Given a string, return a new string with the reversed
 # // order of characters
 # // --- Examples
-# //   reverse('apple') === 'leppa'
+# //   reverse('apple') === 'elppa'
 # //   reverse('hello') === 'olleh'
 # //   reverse('Greetings!') === '!sgniteerG'
 
+
 def reverse_string(string):
+
+    # Empty string
+    result = ""
+    # initialiazing a counter for the while loop
+    count = 0
+
+    while count < len(string):
+        result += string[count]
+        count += 1
     return string
 
 
